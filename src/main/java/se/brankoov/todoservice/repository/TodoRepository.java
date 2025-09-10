15a250b9f2adcd7f0a3d3bea71786c337b7d219d
Branko <banne_nikolic@hotmail.com>
2025-09-10 18:50:49 +0200

    feat: add validation, global exception handling and refactor to packages

--- src/main/java/se/brankoov/todoservice/repository/TodoRepository.java
+++ src/main/java/se/brankoov/todoservice/repository/TodoRepository.java
@@ -1,6 +1,7 @@
-package se.brankoov.todoservice.TodoPackage;
+package se.brankoov.todoservice.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import se.brankoov.todoservice.entity.Todo;
 
 public interface TodoRepository extends MongoRepository<Todo, String> {
 }
